652dc2adb90891549ce5797308b96bd6840f8022
venky101356 <venkateshmetri9741@gmail.com>
2022-08-17 17:38:08 +0530

    Src uploaded

---
+++ src/main/java/com/tyss/person/dto/Pan.java
@@ -0,0 +1,13 @@
+package com.tyss.person.dto;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Pan {
+   
+	private int id;
+	private String fatherName;
+	private String panNumber;
+	
+
+}
